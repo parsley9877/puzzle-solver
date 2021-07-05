1a9374877566e576c83013a94016d8a511b1f428
parsley9877 <parsaassadi2@gmail.com>
2021-07-05 03:52:56 -0700

    testing RGB scrambler done

--- test/scrambler.py
+++ test/scrambler.py
@@ -10,6 +10,7 @@ class ImageScrambler():
         self.block_shape = block_shape
 
     def scramble(self, image: np.ndarray) -> np.ndarray:
+        assert image.ndim == 3
         horizontal_num = self.shape[1] // self.block_shape[1]
         vertical_num = self.shape[0] // self.block_shape[0]
         scrambled_image = np.zeros_like(image)
@@ -17,14 +18,14 @@ class ImageScrambler():
         rand_segments = np.random.choice(num_segments, size=num_segments, replace=False)
         patches = []
         for init_segment, dist_segment in enumerate(rand_segments):
-            init_patch = self.get_patch(init_segment)
-            dist_patch = self.get_patch(dist_segment)
+            init_patch = self._get_patch(init_segment)
+            dist_patch = self._get_patch(dist_segment)
             new_segment = image[init_patch[0]:init_patch[0] + self.block_shape[0], init_patch[1]:init_patch[1] + self.block_shape[1], :]
             scrambled_image[dist_patch[0]:dist_patch[0] + self.block_shape[0], dist_patch[1]:dist_patch[1] + self.block_shape[1], :] = new_segment
 
         return scrambled_image
 
-    def get_patch(self, segment_num: int) -> Tuple[int, int]:
+    def _get_patch(self, segment_num: int) -> Tuple[int, int]:
         horizontal_num = self.shape[1] / self.block_shape[1]
         vertical_num = self.shape[0] / self.block_shape[0]
         row_patch = segment_num // horizontal_num
--- test/scrambler_test.py
+++ test/scrambler_test.py
@@ -3,35 +3,68 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+from typing import Tuple
 from scrambler import ImageScrambler
+
 print("TensorFlow version: {}".format(tf.__version__))
 print("Eager execution: {}".format(tf.executing_eagerly()))
 
-mnist = keras.datasets.mnist
-(X, y), (X_test, y_test) = mnist.load_data()
-X = X.reshape(X.shape[0], 28, 28, 1)
-n = 10
-segment_shape = (14, 14, 1)
-sampled_image = (((X[10].reshape(28, 28, 1) / 255.0) - 0.5) / 0.5).astype(np.float32)
 
+def write_summary(path: str, sampled_image: np.ndarray, scrambled_image: np.ndarray, segment_shape: Tuple) -> None:
+
+    if not os.path.exists(os.path.join(path, 'Scrambled Image')):
+        os.mkdir(os.path.join(path, 'Scrambled Image'))
+
+    if not os.path.exists(os.path.join(path, 'Log Scrambler')):
+        os.mkdir(os.path.join(path, 'Log Scrambler'))
+
+    fig1, ax1 = plt.subplots()
+    if len(sampled_image.shape) == 2:
+        ax1.imshow(sampled_image.reshape([sampled_image.shape[0], sampled_image.shape[1], 1]))
+    else:
+        ax1.imshow(sampled_image.reshape([sampled_image.shape[0], sampled_image.shape[1], sampled_image.shape[2]]))
+    fig1.savefig(os.path.join(path, 'Scrambled Image', 'original.png'))
+
+    fig2, ax2 = plt.subplots()
+    if len(scrambled_image.shape) == 2:
+        ax2.imshow(scrambled_image.reshape([scrambled_image.shape[0], scrambled_image.shape[1], 1]))
+    else:
+        ax2.imshow(scrambled_image.reshape([scrambled_image.shape[0], scrambled_image.shape[1], scrambled_image.shape[2]]))
+    fig2.savefig(os.path.join(path, 'Scrambled Image', 'scrambled.png'))
+
+    log_obj = open(os.path.join(path, 'Log Scrambler', 'log.txt'), mode='w')
+    log_obj.write('Original Image Shape: ' + str(sampled_image.shape) + '\n')
+    log_obj.write('Segments Shape: ' + str(segment_shape) + '\n')
+    log_obj.close()
+
+def get_one_sample(dataset) -> np.ndarray:
+    (X, y), (X_test, y_test) = dataset.load_data()
+    n = np.random.choice(X.shape[0], 1)
+    if len(X.shape) == 4:
+        X = X.reshape(X.shape[0], X.shape[1], X.shape[2], X.shape[3])
+        sampled_image = (((X[n[[0]]].reshape(X.shape[1], X.shape[2], X.shape[3]) / 255.0) - 0.5) / 0.5).astype(np.float32)
+    else:
+        X = X.reshape(X.shape[0], X.shape[1], X.shape[2], 1)
+        sampled_image = (((X[n[[0]]].reshape(X.shape[1], X.shape[2], 1) / 255.0) - 0.5) / 0.5).astype(np.float32)
+    return sampled_image
+
+
+if not os.path.exists('./cifar10-results'):
+    os.mkdir('cifar10-results')
+cifar = keras.datasets.cifar10
+n = 76
+segment_shape = (2, 2, 3)
+sampled_image = get_one_sample(cifar)
+scrambler_object = ImageScrambler(shape=(32, 32), block_shape=segment_shape)
+scrambled_image = scrambler_object.scramble(sampled_image)
+write_summary(path='./cifar10-results', sampled_image=sampled_image, scrambled_image=scrambled_image, segment_shape=segment_shape)
+
+if not os.path.exists('./mnist-results'):
+    os.mkdir('mnist-results')
+mnist = keras.datasets.mnist
+n = 32
+segment_shape = (7, 7, 1)
+sampled_image = get_one_sample(mnist)
 scrambler_object = ImageScrambler(shape=(28, 28), block_shape=segment_shape)
 scrambled_image = scrambler_object.scramble(sampled_image)
-
-if not os.path.exists('./Scrambled Image'):
-    os.mkdir('./Scrambled Image')
-
-if not os.path.exists('./Log Scrambler'):
-    os.mkdir('./Log Scrambler')
-
-fig1, ax1 = plt.subplots()
-ax1.imshow(sampled_image.reshape([28, 28]), cmap='gray')
-fig1.savefig('./Scrambled Image/original.png')
-
-fig2, ax2 = plt.subplots()
-ax2.imshow(scrambled_image.reshape([28, 28]), cmap='gray')
-fig2.savefig('./Scrambled Image/scrambled.png')
-
-log_obj = open('./Log Scrambler/log.txt', mode='w')
-log_obj.write('Original Image Shape: ' + str(sampled_image.shape) + '\n')
-log_obj.write('Segments Shape: ' + str(segment_shape) + '\n')
-log_obj.close()
+write_summary(path='./mnist-results', sampled_image=sampled_image, scrambled_image=scrambled_image, segment_shape=segment_shape)
